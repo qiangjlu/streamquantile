8040ea61df4784fa1561e503e181bd7cd7c827f7
qiangjlu <qiang.ma.chn@gmail.com>
2016-09-23 22:41:12 -0700

    Update Frugal-1U.py

--- Frugal-1U.py
+++ Frugal-1U.py
@@ -8,26 +8,25 @@
   publisher={Springer}
 }
 '''
-import random
+import random as rand
+import numpy as np
 
-class FindQuantile1U:
+class Frugal1U:
     def __init__(self, q):
         self.M = 0
         self.q = q
-        self.label = "FindpQuantile-1U"
-        self.ests = [self.label] ### record estimation history
-        self.rankerrs = [self.labelerr]
+        self.label = "Frugal1U"
+        self.ests = [] ### record estimation history
+        self.rankerrs = []
     
     def GetEstimate(self):
         return self.M
     
     def Update(self, item):
         item = int(item)
-        if item > self.M:
-            if random.random() > 1 - self.q:
+        if item > self.M and rand.random() > 1 - self.q:
                 self.M += 1
-        elif item < self.M:
-            if random.random() > self.q:
+        elif item < self.M and rand.random() > self.q:
                 self.M -= 1
 
     def Record(self, est=None):
@@ -35,3 +34,15 @@ class FindQuantile1U:
     
     def RecordRankError(self, error):
         self.rankerrs.append(error)
+        
+mu, sigma = 5000, 500
+data = [int(x) for x in np.random.normal(mu, sigma, 100000)]
+q = 0.5
+
+print 'Data true '+str(q)+'-quantile', sorted(data)[int(len(data)*q)]
+
+frugal1u = Frugal1U(q)
+for i in range(len(data)):
+    if i % 10000 == 0:
+        print '\tEstimate:', frugal1u.GetEstimate()
+    frugal1u.Update(data[i])
